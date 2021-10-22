f2cee42e80e01fc52b84c3db90bc63259aab141d
zhamilya <zhamilya.saparova@nu.edu.kz>
2021-10-22 15:37:54 +0600

    Updated!

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-from dynaconf import Dynaconf
+# from dynaconf import Dynaconf
+from box import Box
+import os
 
-settings = Dynaconf(
-    settings_files=['settings.yaml'],
-    environments=True,
-    load_dotenv=True,
-)
+settings = Box.from_yaml(filename='settings.yaml')[os.environ['ENV_FOR_DYNACONF']]
+# settings = Dynaconf(
+#     settings_files=['settings.yaml'],
+#     environments=True,
+#     load_dotenv=True,
+# )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load this files in the order.
